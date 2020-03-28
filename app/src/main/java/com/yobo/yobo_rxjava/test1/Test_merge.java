94b5658857dffb526423ac5787adb894132cb80e
zhangboshi <zhangboshi@le.com>
2020-03-28 10:13:35 +0800

    RxJava学习第一章

---
+++ app/src/main/java/com/yobo/yobo_rxjava/test1/Test_merge.java
@@ -0,0 +1,46 @@
+package com.yobo.yobo_rxjava.test1;
+
+import io.reactivex.rxjava3.core.Observable;
+
+/**
+ * Created by ZhangBoshi
+ * on 2020-03-26
+ */
+public class Test_merge {
+
+    public static void main(String[] args) {
+
+        testMerge();
+    }
+
+    private static void testMerge() {
+
+        Observable o1 = Observable.create(s -> {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    s.onNext("one");
+                    s.onNext("two");
+                    s.onNext("three");
+                }
+            }).start();
+
+        });
+        Observable o2 = Observable.create(s -> {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    s.onNext("four");
+                    s.onNext("five");
+                    s.onNext("six");
+                }
+            }).start();
+        });
+
+        Observable o3 = Observable.merge(o1, o2);
+
+        o3.subscribe(System.out::println);
+
+    }
+}
